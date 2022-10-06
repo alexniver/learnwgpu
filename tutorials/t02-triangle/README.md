5657f02bbc86f4aeb21a71948f053f455aab7b79
Alexniver <zhulin10541@gmail.com>
2022-10-07 06:42:08 +0800

    add buffer version triangle

---
+++ tutorials/t02-triangle/README.md
@@ -0,0 +1,7 @@
+```
+cargo run --release -p t02-triangle -- -v v1
+```
+
+```
+cargo run --release -p t02-triangle -- -v v2
+```
--- tutorials/t02-triangle/src/main.rs
+++ tutorials/t02-triangle/src/main.rs
@@ -1,21 +1,41 @@
+use clap::Parser;
 use tracing::{info, Level};
-use wgpu::{include_wgsl, Backends, Instance};
+use wgpu::{include_wgsl, util::DeviceExt, Backends, Instance};
 use winit::{
     event::{ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
     window::{Window, WindowBuilder},
 };
 
+#[derive(Parser)]
+struct Args {
+    #[arg(short, long, default_value_t = String::from("v1"))]
+    version: String,
+}
+
 fn main() {
+    let args = Args::parse();
+
     tracing_subscriber::fmt().with_max_level(Level::INFO).init();
 
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
 
-    pollster::block_on(run(event_loop, window));
+    match args.version.as_str() {
+        "v1" => {
+            pollster::block_on(run_v1(event_loop, window));
+        }
+        "v2" => {
+            pollster::block_on(run_v2(event_loop, window));
+        }
+        _ => {
+            info!("invalid version, exit")
+        }
+    }
 }
 
-async fn run(event_loop: EventLoop<()>, window: Window) {
+// 硬编码，vertex和color信息都在shader文件中
+async fn run_v1(event_loop: EventLoop<()>, window: Window) {
     let size = window.inner_size();
 
     let instance = Instance::new(Backends::all());
@@ -41,7 +61,7 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
         .await
         .expect("Fail to create device");
 
-    let shader = device.create_shader_module(include_wgsl!("shader.wgsl"));
+    let shader = device.create_shader_module(include_wgsl!("shader-v1.wgsl"));
     let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
         label: None,
         bind_group_layouts: &[],
@@ -112,6 +132,182 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
                     });
 
                     rpass.set_pipeline(&render_pipeline);
+                    rpass.draw(0..3, 0..1);
+                }
+
+                queue.submit(Some(encoder.finish()));
+                frame.present();
+            }
+            Event::WindowEvent { window_id, event } if window_id == window.id() => {
+                match event {
+                    WindowEvent::Resized(size) => {
+                        config.width = size.width;
+                        config.height = size.height;
+                        surface.configure(&device, &config);
+
+                        window.request_redraw(); // for macos, need redraw when size change
+                    }
+
+                    WindowEvent::CloseRequested
+                    | WindowEvent::KeyboardInput {
+                        input:
+                            KeyboardInput {
+                                state: ElementState::Pressed,
+                                virtual_keycode: Some(VirtualKeyCode::Escape),
+                                ..
+                            },
+                        ..
+                    } => {
+                        info!("exit");
+                        *control_flow = ControlFlow::Exit
+                    }
+                    _ => {}
+                }
+            }
+            _ => {}
+        }
+    });
+}
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+struct Vertex {
+    position: [f32; 3],
+    color: [f32; 3],
+}
+
+impl Vertex {
+    const ATTRIBUTS: [wgpu::VertexAttribute; 2] =
+        wgpu::vertex_attr_array![0=>Float32x3, 1=> Float32x3];
+    fn buffer_layout<'a>() -> wgpu::VertexBufferLayout<'a> {
+        wgpu::VertexBufferLayout {
+            array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Vertex,
+            attributes: &Self::ATTRIBUTS,
+        }
+    }
+}
+
+const VERTICES: &[Vertex] = &[
+    Vertex {
+        position: [0.0, 0.5, 0.0],
+        color: [1.0, 0.0, 0.0],
+    },
+    Vertex {
+        position: [-0.5, -0.5, 0.0],
+        color: [0.0, 1.0, 0.0],
+    },
+    Vertex {
+        position: [0.5, -0.5, 0.0],
+        color: [0.0, 0.0, 1.0],
+    },
+];
+
+// 使用buffer, 将vertex信息传到shader文件中
+async fn run_v2(event_loop: EventLoop<()>, window: Window) {
+    let size = window.inner_size();
+
+    let instance = Instance::new(Backends::all());
+    let surface = unsafe { instance.create_surface(&window) };
+    let adapter = instance
+        .request_adapter(&wgpu::RequestAdapterOptions {
+            power_preference: wgpu::PowerPreference::default(),
+            force_fallback_adapter: false,
+            compatible_surface: Some(&surface),
+        })
+        .await
+        .expect("Failed to find an appropriate adapter");
+
+    let (device, queue) = adapter
+        .request_device(
+            &wgpu::DeviceDescriptor {
+                label: None,
+                features: wgpu::Features::empty(),
+                limits: wgpu::Limits::downlevel_defaults(),
+            },
+            None,
+        )
+        .await
+        .expect("Fail to create device");
+
+    let shader = device.create_shader_module(include_wgsl!("shader-v2.wgsl"));
+    let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+        label: None,
+        bind_group_layouts: &[],
+        push_constant_ranges: &[],
+    });
+
+    let swapchain_format = surface.get_supported_formats(&adapter)[0];
+
+    let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+        label: None,
+        layout: Some(&pipeline_layout),
+        vertex: wgpu::VertexState {
+            module: &shader,
+            entry_point: "vs_main",
+            buffers: &[Vertex::buffer_layout()],
+        },
+        fragment: Some(wgpu::FragmentState {
+            module: &shader,
+            entry_point: "fs_main",
+            targets: &[Some(swapchain_format.into())],
+        }),
+        primitive: wgpu::PrimitiveState::default(),
+        depth_stencil: None,
+        multisample: wgpu::MultisampleState::default(),
+        multiview: None,
+    });
+
+    let mut config = wgpu::SurfaceConfiguration {
+        usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+        format: swapchain_format,
+        width: size.width,
+        height: size.height,
+        present_mode: wgpu::PresentMode::Fifo,
+        alpha_mode: surface.get_supported_alpha_modes(&adapter)[0],
+    };
+
+    surface.configure(&device, &config);
+
+    let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: Some("Vertex Buffer"),
+        contents: bytemuck::cast_slice(VERTICES),
+        usage: wgpu::BufferUsages::VERTEX,
+    });
+
+    event_loop.run(move |event, _, control_flow| {
+        let _ = (&instance, &adapter, &shader, &pipeline_layout);
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            Event::RedrawRequested(_) => {
+                let frame = surface
+                    .get_current_texture()
+                    .expect("Fail to request next swap chain texture");
+
+                let view = frame
+                    .texture
+                    .create_view(&wgpu::TextureViewDescriptor::default());
+
+                let mut encoder =
+                    device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+
+                {
+                    let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                        label: None,
+                        color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                            view: &view,
+                            resolve_target: None,
+                            ops: wgpu::Operations {
+                                load: wgpu::LoadOp::Clear(wgpu::Color::BLACK),
+                                store: true,
+                            },
+                        })],
+                        depth_stencil_attachment: None,
+                    });
+
+                    rpass.set_pipeline(&render_pipeline);
+                    rpass.set_vertex_buffer(0, vertex_buffer.slice(..));
                     rpass.draw(0..3, 0..1);
                 }
 
